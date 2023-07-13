40cc11164722eda97f2c7bf0ce729beee17cd9ab
lee000403 <dong000403@gmail.com>
2023-07-13 18:20:14 +0900

    이동환 CRUD 완성본

--- src/main/java/com/yojulab/study_springboot/restapis/CarInforsRestTest.java
+++ src/main/java/com/yojulab/study_springboot/restapis/CarInforsRestTest.java
@@ -1,9 +1,12 @@
 package com.yojulab.study_springboot.restapis;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yojulab.study_springboot.service.CarInforsService;
