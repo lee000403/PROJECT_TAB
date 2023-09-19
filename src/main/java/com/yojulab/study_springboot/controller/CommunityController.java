f6bd8b0e0717d648f73edd79504a8f21dfef651f
dancingkimDH <kimdonghyun0701@gmail.com>
2023-09-19 12:12:57 +0900

    update

--- src/main/java/com/yojulab/study_springboot/controller/CommunityController.java
+++ src/main/java/com/yojulab/study_springboot/controller/CommunityController.java
@@ -11,19 +11,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.yojulab.study_springboot.service.CommunityService;
 import com.yojulab.study_springboot.service.Project_TABService;
 
 @Controller
 @RequestMapping("/TAB_PAGE")
 public class CommunityController {
+    
     @Autowired
     Project_TABService project_TABService;
+
+    @Autowired
+    CommunityService communityService;
 
     @RequestMapping(value = { "/search_community/{currentPage}", "/search_community/" }, method = { RequestMethod.GET,
             RequestMethod.POST })
     public ModelAndView search_community(@PathVariable(required = false) String currentPage, @RequestParam Map params,
             ModelAndView modelAndView) {
-        Object result = project_TABService.selectSearchWithPagination_Community(currentPage,
+        Object result = communityService.selectSearchWithPagination_Community(currentPage,
                 params);
         modelAndView.addObject("params", params);
         modelAndView.addObject("result", result);
@@ -34,7 +39,7 @@ public class CommunityController {
 
     @PostMapping("/community")
     public ModelAndView community(@RequestParam Map params, ModelAndView modelAndView) {
-        Object result = project_TABService.select_community(params);
+        Object result = communityService.select_community(params);
         modelAndView.addObject("params", params);
         modelAndView.addObject("result", result);
 
@@ -52,7 +57,7 @@ public class CommunityController {
     @PostMapping("/community_answer/{UNIQUE_ID}")
     public ModelAndView community_answer(@PathVariable String UNIQUE_ID, @RequestParam Map params,
             ModelAndView modelAndView) {
-        Object result = project_TABService.community_answer(UNIQUE_ID, params);
+        Object result = communityService.community_answer(UNIQUE_ID, params);
         modelAndView.addObject("params", params);
         modelAndView.addObject("result", result);
 
@@ -63,7 +68,7 @@ public class CommunityController {
     @PostMapping("/community_end/{username}")
     public ModelAndView community_end(@PathVariable String username, @RequestParam Map params,
             ModelAndView modelAndView) {
-        Object result = project_TABService.community_end(username, params);
+        Object result = communityService.community_end(username, params);
         modelAndView.addObject("params", params);
         modelAndView.addObject("result", result);
 
@@ -74,7 +79,7 @@ public class CommunityController {
     @PostMapping("/community_update/{POST_ID}")
     public ModelAndView community_update(@PathVariable String POST_ID, @RequestParam Map params,
             ModelAndView modelAndView) {
-        Object result = project_TABService.community_answer(POST_ID, params);
+        Object result = communityService.community_answer(POST_ID, params);
         modelAndView.addObject("params", params);
         modelAndView.addObject("result", result);
 
@@ -85,7 +90,7 @@ public class CommunityController {
     @PostMapping("/community_updateandSelect/{POST_ID}")
     public ModelAndView community_updateandSelect(@PathVariable String POST_ID, @RequestParam Map params,
             ModelAndView modelAndView) {
-        Object result = project_TABService.community_updateandSelect(POST_ID, params);
+        Object result = communityService.community_updateandSelect(POST_ID, params);
         modelAndView.addObject("params", params);
         modelAndView.addObject("result", result);
 
@@ -96,7 +101,7 @@ public class CommunityController {
     @PostMapping("/community_deleteandSelect/{POST_ID}")
     public ModelAndView community_deleteandSelect(@PathVariable String POST_ID, @RequestParam Map params,
             ModelAndView modelAndView) {
-        Object result = project_TABService.community_deleteandSelect(POST_ID, params);
+        Object result = communityService.community_deleteandSelect(POST_ID, params);
         modelAndView.addObject("params", params);
         modelAndView.addObject("result", result);
 
