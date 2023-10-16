a30b884a3a965e3d75e475e900f3aac2aaeaa8ca
dancingkimDH <kimdonghyun0701@gmail.com>
2023-10-16 11:39:26 +0900

    update

--- src/main/resources/static/js/statistics.js
+++ src/main/resources/static/js/statistics.js
@@ -52,7 +52,8 @@ function initializeChart(chartId, label, data) {
                 data: data,
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(255, 218, 185, 0.7)'
             }]
         },
         options: {
